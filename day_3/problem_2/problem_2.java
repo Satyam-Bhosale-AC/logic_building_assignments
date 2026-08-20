bc6e6d7e54757bfc25df2021e5388f00c57418b4
SatyamBhosale2004 <satyambhosale.work@gmail.com>
2026-08-21 01:12:57 +0530

    feat:problem_2 day 3 done

---
+++ day_3/problem_2/problem_2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class problem_2{
+    static void checkAgeCategory(int age){
+        if(age >= 18 && age < 60) {
+            System.out.println("You are an Adult");
+        } else if(age >= 60){
+            System.out.println("You are a Senior Citizen");
+        }else {
+            System.out.println("You are a Minor");
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+        checkAgeCategory(age);
+    }
+}
