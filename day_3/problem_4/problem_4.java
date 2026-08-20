2566d0f17a89e67d8aa691dc87c4a4c6d2b55d78
SatyamBhosale2004 <satyambhosale.work@gmail.com>
2026-08-21 01:34:24 +0530

    feat:pb 4 day 3 done

---
+++ day_3/problem_4/problem_4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class problem_4{
+    static void askForPositiveNumber(){
+        Scanner sc = new Scanner(System.in);
+        int i = 1;
+        do {
+            System.out.println("Enter a positive number: ");
+            i = sc.nextInt();
+            
+        } while(i <= 0);
+
+        System.out.println("You entered a positive number: "+i);
+    }
+    public static void main(String args[]){
+        askForPositiveNumber();
+    }
+}
