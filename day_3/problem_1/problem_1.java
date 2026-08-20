e1abfc23e8b55b3016cef845df80b77ac17eaa5c
SatyamBhosale2004 <satyambhosale.work@gmail.com>
2026-08-21 00:53:31 +0530

    feat:problem 1 day 3 done

---
+++ day_3/problem_1/problem_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class problem_1{
+    static int sumOfTwoNumbers(int a,int b){
+        int sum = a+b;
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = sumOfTwoNumbers(a,b);
+        System.out.println("sum : " + sum);
+        
+    }
+}
