a320eebcf809fe806d35542f47a02e828125481d
SatyamBhosale2004 <satyambhosale.work@gmail.com>
2026-08-22 16:33:27 +0530

    feat:problem 27 day 5 done

---
+++ day_5/problem_27/problem_27.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class problem_27 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s1 = "hello";
+        String s2 = "hello";
+
+        boolean check = (s1 == s2);
+        System.out.println("both variables point to the same object : " + check);
+    }    
+}
