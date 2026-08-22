07acea22dd2b95299a30ecb03ef673e1b0aa9e07
SatyamBhosale2004 <satyambhosale.work@gmail.com>
2026-08-22 16:52:41 +0530

    feat problem 31 day 5 done

---
+++ day_5/problem_31/problem_31.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class problem_31 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = "hello";
+
+        boolean check1 = (s3 == s1  && s2 ==s1);
+        
+        System.out.println("all strings point to same obj : " + check1);
+    }    
+}
