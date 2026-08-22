d5f8623572921d6a3f68340e460ee639de975f70
SatyamBhosale2004 <satyambhosale.work@gmail.com>
2026-08-22 15:41:22 +0530

    feat problem 24 day 5 done

---
+++ day_5/problem_24/problem_24.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class problem_24 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s =  sc.nextLine();
+        String reverse = "";
+        for(int i =s.length()-1; i >=0;i--){
+            reverse += s.charAt(i);
+        }
+        System.out.println("Reversed String: " + reverse);
+        
+    }    
+}
